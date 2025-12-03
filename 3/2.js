d2e4838bb8df4e8700fb7750e04db3de02094b30
Sean Ng <sean.ng@xpf.com>
2025-12-03 18:31:03 +0800

    Complete day 3 part 2

--- 3/2.js
+++ 3/2.js
@@ -1,5 +1,25 @@
 const solve = (lines) => {
-    return "todo";
+    return lines.reduce((acc, bank) => {
+        const largestJoltage = getLargestPossibleJoltageForBank(bank.split('').map(c => +c), 12)
+        return acc + largestJoltage
+    }, 0)
+}
+
+const getLargestPossibleJoltageForBank = (bank, batteries) => {
+    if (batteries <= 0) {
+        return 0;
+    }
+    
+    //console.log(bank, batteries)
+    const highest = Math.max(...bank);
+
+    // we want highest as far up as possible (fit as many batteries after as possible)
+    const highestIdx = bank.findIndex(b => b === highest);
+    const batteriesAfter = Math.min(bank.length - highestIdx - 1, batteries - 1);
+
+    return highest * 10 ** batteriesAfter 
+        + getLargestPossibleJoltageForBank(bank.slice(highestIdx + 1), batteriesAfter) 
+        + getLargestPossibleJoltageForBank(bank.slice(0, highestIdx), batteries - 1 - batteriesAfter) * 10 ** (batteriesAfter + 1)
 }
 
 module.exports = solve;
