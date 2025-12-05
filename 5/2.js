c7f73f67834708d96852b380821f5dbfdb2cf465
snss231 <e0543670@u.nus.edu>
2025-12-05 23:14:12 +0800

    Solve day 5 part 2

--- 5/2.js
+++ 5/2.js
@@ -1,5 +1,32 @@
 const solve = (lines) => {
-    return "todo";
+    let res = 0;
+    const ranges = lines
+        .slice(0, lines.indexOf(''))
+        .map(line => line
+            .split('-')
+            .map(c => +c))
+        .toSorted((a, b) => a[0] - b[0]);
+    const mergedRanges = [];
+
+    let [start, end] = ranges[0];
+
+    for (const [s, e] of ranges.slice(1)) {
+        console.log(s, e, end)
+        if (s <= end) {
+            end = Math.max(e, end);
+            continue;
+        } 
+        mergedRanges.push([start, end])
+        start = s;
+        end = e;
+    }
+    mergedRanges.push([start, end])
+    console.log(mergedRanges)
+    for (const [start, end] of mergedRanges) {
+        res += (end - start + 1);
+    }
+    return res;
 }
+
 
 module.exports = solve;
