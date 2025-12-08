777624569029da8cee5a54755e815af37d1a6e95
Sean Ng <sean.ng@xpf.com>
2025-12-08 10:28:14 +0800

    Solve day 7 part 2

--- 7/2.js
+++ 7/2.js
@@ -1,5 +1,18 @@
 const solve = (lines) => {
-    return "todo";
+    let beams = [...lines[0]].map(c => c === '.' ? 0 : 1);
+    for (const line of lines.slice(1)) {
+        let nextBeam = beams.map(_ => 0);
+        for (const [i, b] of beams.entries()) {
+            if (line[i] === '^') {
+                nextBeam[i - 1] += b;
+                nextBeam[i + 1] += b;
+            } else {
+                nextBeam[i] += b;
+            }
+        }
+        beams = nextBeam;
+    }
+    return beams.reduce((acc, curr) => curr + acc);
 }
 
 module.exports = solve;
